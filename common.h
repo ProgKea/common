8ca8573319811af70b49f9d849eaae14174112b4
ProgKea <keanu.daniel@e-mail.de>
2023-01-14 15:36:53 +0100

    Initial Commit

---
+++ common.h
@@ -0,0 +1,25 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#define SWAP(T, a, b) \
+    do {              \
+        T t = a;      \
+        a = b;        \
+        b = t         \
+    } while (0)
+
+#define UNIMPLEMENTED(...)                                                      \
+    do {                                                                        \
+        printf("%s:%d: UNIMPLEMENTED: %s \n", __FILE__, __LINE__, __VA_ARGS__); \
+        exit(1);                                                                \
+    } while (0)
+
+#define UNUSED(x) (void)(x)
+
+#define return_defer(value) \
+    do {                    \
+        result = (value);   \
+        goto defer;         \
+    } while (0)
+
+#endif
